cb95c6c94a0af2d34705f34867df54479f9de4f0
carlin201 <cp0108079@gmail.com>
2026-07-12 21:58:00 -0400

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,16 +1,21 @@
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
+
   apiKey: "AIzaSyCj52UNZGsiUsDCw7B41Di8xZubTn87zZE",
+
   authDomain: "ctr-fitness-app.firebaseapp.com",
+
   projectId: "ctr-fitness-app",
+
   storageBucket: "ctr-fitness-app.firebasestorage.app",
+
   messagingSenderId: "871828540628",
+
   appId: "1:871828540628:web:9101e90a6e460efa443f41"
+
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export default app;
